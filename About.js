f708844fed7ae3df7e5cdd3c8dd319d8231c98d1
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2023-05-30 11:15:02 +0300

    Update About.js

--- About.js
+++ About.js
@@ -3,7 +3,6 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
 }
 
 /*================scroll section active link===============*/
