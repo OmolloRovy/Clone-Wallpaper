17c5e0a63efb729089e0e6ba94bf68da9d77ca27
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2023-09-13 10:52:14 +0300

    Update clone.jsx

--- clone.jsx
+++ clone.jsx
@@ -12,6 +12,7 @@ if(err) throw err;
   fs.appendFile(path.join(__dirname, 'files', 'append.txt', 'testing text'),(err)=>{
 if (err) throw err;
   console.log('append complete');   
+                
   fs.rename(path.join(__dirname,'files', 'append.txt', 'newrename.txt', )(err)=>{
     if(err) throw err;
     consloe.log('rename complete');
