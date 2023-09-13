806cd605bbdb622d6aca97d92b09ebddc16e30bb
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2023-09-13 10:16:57 +0300

    Update clone.jsx

--- clone.jsx
+++ clone.jsx
@@ -3,5 +3,5 @@ const fsPromises = require('fs').promises;
 const path =require('path');
 fs.readFile(path.join(__dirname, 'files','fikes.txt'),'utf8'(data, err)=>{
 if (err) throw err;
-  
+  console.log(log);
 });
