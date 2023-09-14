045db3bc4274c4d20f417d0711498019ea8f9a7d
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2023-09-14 09:54:57 +0300

    Update clone.jsx

--- clone.jsx
+++ clone.jsx
@@ -34,7 +34,9 @@ const fileOps = asycn()=>{
     try{
       const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), utf8);
       console.log(data);
-  await fsPromises.writeFile(path.join(__dirname,'files','promisesWrite.txt'),data)
+  await fsPromises.writeFile(path.join(__dirname,'files','promisesWrite.txt'),data);
+  await fsPromises.appendFile(path.join(__dirname,'files','promisesWrite.txt','\n\n the big init'));
+  
     }catch{
       console.log(err)
     }
