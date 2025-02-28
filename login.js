b1eb7878e4e5a2b4031aa55e2fb3bcac675bbea1
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2025-02-28 09:08:07 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-function () {
+function validation () {
     if (document.Formfill.Email.value == "") {
         document.getElementById("result").innerHTML = "Enter your Email*";
         return false;
