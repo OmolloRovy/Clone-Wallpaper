f4b5319a36de97d42a9c56ffee56da554e98c562
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2025-03-05 08:55:10 +0300

    Update clone.jsx

--- clone.jsx
+++ clone.jsx
@@ -1,5 +1,5 @@
 const fs = require ('fs');
-const fsPromises = require('fs').promises
+const fsPromises = require('fs').promises;
 const fs =require('fs');
 const fsPromises = require('fs').promises;
 const path =require('path');
